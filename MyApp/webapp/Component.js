cfe9987e49674179782c46ab56b263e28ec497f5
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-06-24 21:50:08 +0300

    clean up

--- MyApp/webapp/Component.js
+++ MyApp/webapp/Component.js
@@ -10,10 +10,7 @@ sap.ui.define([
         },
 
         init : function () {
-            // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-
-            // additional initialization can be done here
         }
 
     });
--- MyApp/webapp/controller/App.controller.js
+++ MyApp/webapp/controller/App.controller.js
@@ -2,12 +2,9 @@ sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
         "sap/m/MessageToast"
-    ], function (Controller, MessageToast) {
+    ], function (Controller) {
 
         Controller.extend("opensap.myapp.controller.App", {
-            onShowHello: function () {
-                MessageToast.show("Hello openSAP!");
-            },
 
             onSave: function () {
 
@@ -30,47 +27,6 @@ sap.ui.define(
                         new sap.m.MessageToast.show("Error while adding the product");
                     }
                 });
-                /*
-                $.ajax({
-                    type: "POST",
-                    data: "POST CONTENT DATA",
-                    crossDomain: true,
-                    url: "http://mydomain.coim/service/endpooint",
-                headers: {'key1':'value1','key2':'value2'},
-                contentType: "application/json",
-                    success: function (res, status, xhr) {
-                    //success code
-                        console.log("SUCCESS!");
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    console.log("POST request error: " + textStatus + errorThrown);
-                }
-            });
-            */
-
-                /*/////////////////////////////////////
-                var fnSuccess = function () {
-                    // this._setBusy(false);
-                    MessageToast.show(this._getText("changesSentMessage"));
-                    this._setUIChanges(false);
-                }.bind(this);
-
-                var fnError = function (oError) {
-                    // this._setBusy(false);
-                    this._setUIChanges(false);
-                    MessageBox.error(oError.message);
-                }.bind(this);
-
-                // this._setBusy(true); // Lock UI until submitBatch is resolved.
-                console.log('this.getView()>>>', this.getView());
-                this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
-                this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
-                */
             },
-
-            // _setBusy : function (bIsBusy) {
-            //     var oModel = this.getView().getModel("appView");
-            //     oModel.setProperty("/busy", bIsBusy);
-            // }
         });
     });
